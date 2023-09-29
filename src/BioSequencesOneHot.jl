7d8e608364fa7a9027d60771eaa539c1060ced51
Jorge FdCD <cossio@users.noreply.github.com>
2023-09-29 15:23:58 +0200

    Files generated by PkgTemplates
    PkgTemplates version: 0.7.45

---
+++ src/BioSequencesOneHot.jl
@@ -0,0 +1,5 @@
+module BioSequencesOneHot
+
+# Write your package code here.
+
+end
---
+++ test/runtests.jl
@@ -0,0 +1,6 @@
+using BioSequencesOneHot
+using Test
+
+@testset "BioSequencesOneHot.jl" begin
+    # Write your tests here.
+end
